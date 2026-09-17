87a669e4cd63a23ad17d4524aab76587605d818d
Hau576 <trantrunghau576@gmail.com>
2026-09-17 08:34:23 +0700

    Tuan 1 / Bai 01 / Viet chuong trinh nhap xuat ho ten va thao tac voi ma MSIL

---
+++ Bai01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Bai01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao ho ten cua ban: ");
+            string hoten = Console.ReadLine();
+            Console.WriteLine("Ho va ten cua ban la: " + hoten);
+        }
+    }
+}
+
+
+
+
+
