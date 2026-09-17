c15593c537ecc5bc4d3cb9989ddb1224505039e0
Hau576 <trantrunghau576@gmail.com>
2026-09-17 08:46:57 +0700

    Tuan 1 / Bai 08 / Phuong thuc hoan vi hai so thuc su dung tham chieu ref

---
+++ Bai08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai08
+{
+    class Program
+    {
+        static void hoanvi(ref double x, ref double y)//ham hoan vi 2 so thuc
+        {
+            double t = x; //bien tam luu x
+            x = y;//gan x vao y
+            y = t;//gan lai x ban dau(tam) cho y
+        }
+
+        static void Main()
+        {
+            Console.Write("Nhap vao x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap vao y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nVi tri ban dau(truoc hoan vi): {x}  {y}");//vi tri ban dau
+
+            hoanvi(ref x, ref y);//goi lai ham hoan vi
+
+            Console.WriteLine($"\nKhi da doi cho (sau hoan vi): {x}  {y}");//in ra vi tri sau khi hoan vi
+        }
+    }
+}
+
