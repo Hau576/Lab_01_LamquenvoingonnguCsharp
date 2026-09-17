2d9b33b5d95293b36c8bb87e755748555291535c
Hau576 <trantrunghau576@gmail.com>
2026-09-17 08:35:30 +0700

    Tuan 1 / Bai 02 / Nhap va xuat chuoi theo dinh dang

---
+++ Bai02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Bai02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban : ");//nguoi dung nhap vao ho ten
+            string hoten = Console.ReadLine();//luu ho ten cua ho nhap vao o dang chuoi(string)
+            Console.Write($"Chao ban {hoten}!");//in ra ket qua 
+        }
+    }
+}
