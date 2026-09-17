6bd21a236c67215b6f55d112fe9004bb90a6acf4
Hau576 <trantrunghau576@gmail.com>
2026-09-17 08:36:22 +0700

    Tuan 1 / Bai 03 / Nhap so nguyen va tinh luy thua x mu y

---
+++ Bai03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Bai03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen x: ");
+            int x = int.Parse(Console.ReadLine());//nguoi dung nhap vao x o dang so nguyen(int)
+            Console.Write("Nhap so nguyen y: ");
+            int y = int.Parse(Console.ReadLine());//nguoi dung nhap vao y o dang so nguyen(int)
+            Console.WriteLine($"Ket qua cua {x} mu {y} la: "+Math.Pow(x, y));//dung Math.Pow de tinh x^y roi in ra ket qua
+        }
+    }
+}
+
+
