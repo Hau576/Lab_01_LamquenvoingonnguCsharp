1e582ef5aa269d157e0f839b590a77e344b4c3b8
Hau576 <trantrunghau576@gmail.com>
2026-09-17 08:49:42 +0700

    Tuan 1 / Bai 10 / Phuong thuc thanh vien kiem tra chuoi doi xung

---
+++ Bai10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Bai10
+{
+    static bool chuoidoixung(string s)
+    {
+        int left = 0;//con tro bat dau tu ben trai
+        int right = s.Length - 1;//con tro bat dau tu ben phai
+
+        while (left < right)//vong lap dung lai khi con tro ben trai lon hon hoac bang con tro ben phai
+        {
+            if (s[left] != s[right]) //neu 2 ky tu khac nhau thi tra ve false
+            {
+                return false;
+            }
+            left++;//tang con tro ben trai len 1
+            right--;//giam con tro ben phai xuong 1
+        }
+        return true; //tra ve true neu da la chuoi doi xung
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap vao mot chuoi can kiem tra: ");
+        string chuoi = Console.ReadLine();
+
+        if (chuoidoixung(chuoi))
+        {
+            Console.WriteLine($"Chuoi  \"{chuoi}\" la chuoi doi xung");//in ra neu la chuoi doi xung
+        }
+        else
+        {
+            Console.WriteLine($"Chuoi \"{chuoi}\" khong phai la chuoi doi xung");//in ra neu khong phai la chuoi doi xung
+        }
+    }
+}
